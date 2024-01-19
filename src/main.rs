ec7e45600d18e241728330ccaef9bab894eb461e
Marcus <hoffmannmarcus2@gmail.com>
2024-01-19 19:53:21 +0100

    First version of repl. reads file.txt and prints all tokens

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
-use monkey::lexer::Lexer;
-use monkey::token::{Token, TokenType};
+use monkey::repl;
 
 
 fn main() {
-    let mut testlexer = Lexer::new(String::from("let== 400; ,djflad;"));
-    let testtoken  = Token::new(TokenType::ASSIGN,String::from("="));
-    println!("{:?}", testtoken.tokentype);
-    println!("{:?}", testtoken.literal);
-    println!("{:?}", testlexer.ch);
-    println!("{:?}", testlexer.next_token());
-    println!("{:?}", testlexer.next_token());
-    println!("{:?}", testlexer.next_token());
-    println!("{:?}", testlexer.next_token());
-    println!("{:?}", testlexer.next_token());
-    println!("{:?}", testlexer.next_token());
-    println!("{:?}", testlexer.input);
-
-
+    let args = std::env::args();
+    repl::start(args);
 }
--- src/repl.rs
+++ src/repl.rs
@@ -1,2 +1,31 @@
-use crate::token::{Token, TokenType};
+use crate::token::TokenType;
 use crate::lexer::Lexer;
+use std::fs;
+
+pub fn start_interactive() {
+    println!(">> ");
+    
+}
+
+pub fn start(mut args: impl Iterator<Item = String>) {
+
+    args.next();
+
+    let filepath = match args.next() {
+        Some(x) => x,
+        None => panic!("Error: could not open monkeyfile"),
+    };
+
+    let mut monkey_lexer = match fs::read_to_string(filepath) {
+        Ok(l) => Lexer::new(l),
+        Err(err) => panic!("Could not create lexer. Error: {}", err),
+    };
+
+    loop {
+        let tok = monkey_lexer.next_token();
+        match tok.tokentype {
+            TokenType::EOF => break,
+            _ => println!("{:?}", tok)
+        }
+    } 
+}
